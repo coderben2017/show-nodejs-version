bb28ddc0a1c640d75117b0468ad9275326aae819
jinben <jinben@sugon.com>
2023-02-28 11:03:16 +0800

    🦄 refactor: 整体结构优化

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,37 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { spawn } from 'child_process';
+import { spawnSync } from 'child_process';
 
 let statusBarItem: vscode.StatusBarItem | undefined;
 let refreshCommand: vscode.Disposable | undefined;
+let timer: NodeJS.Timer | undefined;
 
 function getNodeVersion() {
 	if (!statusBarItem) {
 		console.log('[show-node-version]: 无法获取到状态栏实例');
 		return;
 	}
-	const version = (spawn('node', ['--version']) as any)?.output[1];
+	const info = spawnSync('node', ['--version']);
+	const version = info.stdout.toString();
 	statusBarItem.text = `Node ${version}`;
 }
 
 export function activate(context: vscode.ExtensionContext) {
 	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
 	statusBarItem.show();
-	getNodeVersion();
 
-	refreshCommand = vscode.commands.registerCommand('show-nodejs-version.refresh', () => {
-		getNodeVersion();
-	});
+	refreshCommand = vscode.commands.registerCommand('show-nodejs-version.refresh', getNodeVersion);
 
 	context.subscriptions.push(statusBarItem, refreshCommand);
+
+	timer = setInterval(() => {
+		getNodeVersion();
+	}, 1500);
 }
 
 export function deactivate() {
 	statusBarItem?.dispose();
 	refreshCommand?.dispose();
+	clearInterval(timer);
 }
